977547345e7564242978957b7f2fdce519b2e191
devaprasad-n-m <59179637+devaprasad-n-m@users.noreply.github.com>
2021-10-01 10:36:22 +0530

    Update 1_line.py

--- 1_line.py
+++ 1_line.py
@@ -1,88 +1,64 @@
 from OpenGL.GL import *
-from OpenGL.GLU import *     
-from OpenGL.GLUT import *    
-import sys
-import math
+from OpenGL.GLU import *
+from OpenGL.GLUT import *
 
 
-
-def clearScreen():
-    glClearColor(0.0, 0.0, 0.0, 1.0)
-    gluOrtho2D(-1.0, 1.0,-1.0,1.0)
-
-
-def horizontal():
+def ClearScreen():
+    glClearColor(0.0, 0.0, 0.0, 0.0)
+    gluOrtho2D(-100.0, 100.0, -100.0, 100.0)
     glClear(GL_COLOR_BUFFER_BIT)
-    glColor3f(0.0,1.0,0.0)
-    glPointSize(10.0)
-    glBegin(GL_LINES)       
-    glVertex2f(x1, y)
-    glVertex2f(x2, y) 
-    glEnd()
-    glFlush()
- 
-def vertical():
-    glClear(GL_COLOR_BUFFER_BIT)
-    glColor3f(0.0,1.0,0.0)
-    glPointSize(10.0)
-    glBegin(GL_LINES)       
-    glVertex2f(x, y1)
-    glVertex2f(x, y2) 
+    glColor3f(0.0, 0.0, 1.0)
+    glPointSize(5.0)
+
+
+def SetPixel(x, y):
+    glBegin(GL_POINTS)
+    glVertex2f(x, y)
     glEnd()
     glFlush()
 
 
-def diagonal():
-    glClear(GL_COLOR_BUFFER_BIT)
-    glColor3f(0.0,1.0,0.0)
-    glPointSize(10.0)
-    glBegin(GL_LINES)       
-    glVertex2f(a, a)
-    glVertex2f(b, b) 
-    glEnd()
-    glFlush()
+def MidPointLine(x1, y1, x2, y2):
+    if x2 > x1:
+        x, y, xend = x1, y1, x2
+        dx, dy = x2-x1, y2-y1
+    else:
+        x, y, xend = x2, y2, x1
+        dx, dy = x1-x2, y1-y2
+
+    SetPixel(x, y)
+    P = dy-dx/2
+
+    while x < xend:
+        x = x+1
+        if P < 0:
+            P += dy
+        else:
+            y = y+1
+            P += dy-dx
+        SetPixel(x, y)
+
+def Main():
+     choice =0 
+     while choice != 2:
+        choice = int(input("Enter \n1) Plot a Line \n2) exit \n"))
+        if choice ==1:
+             x1 = float(input("X1 Co-ordinate:"))
+             y1 = float(input("Y1 Co-ordinate:"))
+             x2 = float(input("X2 Co-ordinate:"))
+             y2 = float(input("Y2 Co-ordinate:"))
+             print("starting window....")
+             glutInit()
+             glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE)
+             glutInitWindowSize(500, 500)
+             glutInitWindowPosition(200, 200)
+             glutCreateWindow("Lin using Midpoint-Algorithm")
+             glutDisplayFunc(lambda:MidPointLine(x1, y1, x2, y2))
+             ClearScreen()
+             glutMainLoop()
+
+        else :
+            print("Invalid Choice")
 
 
-
-s=int(input("Enter\n 1 for horizontal line \n 2 for vertical line\n 3 for diagonal line\n"))
-if s==1:
-    x1 = float(input('Enter x1 value : '))/100
-    x2 = float(input('Enter x2 value : '))/100
-    y = float(input('Enter y value : '))/100
-
-    glutInit(sys.argv)
-    glutInitDisplayMode(GLUT_RGB)
-    glutCreateWindow("Horizontal Line")
-    glutInitWindowSize(500, 500)
-    glutInitWindowPosition(50, 50)
-    glutDisplayFunc(horizontal)
-    clearScreen()
-    glutMainLoop()
-elif s==2:
-    x = float(input('Enter x value : '))/100
-    y1 = float(input('Enter y1 value : '))/100
-    y2 = float(input('Enter y2 value : '))/100
-    glutInit(sys.argv)
-    glutInitDisplayMode(GLUT_RGB)
-    glutCreateWindow("Vertical Line")
-    glutInitWindowSize(500, 500)
-    glutInitWindowPosition(50, 50)
-    glutDisplayFunc(vertical)
-    clearScreen()
-    glutMainLoop()
-elif s==3:
-    a = float(input('Enter a value : '))/100
-    b = float(input('Enter b value : '))/100
-    glutInit(sys.argv)
-    glutInitDisplayMode(GLUT_RGB)
-    glutCreateWindow("Diagonal")
-    glutInitWindowSize(500, 500)
-    glutInitWindowPosition(50, 50)
-    glutDisplayFunc(diagonal)
-    clearScreen()
-    glutMainLoop()
-
-        
-
-    
-    
+Main()
